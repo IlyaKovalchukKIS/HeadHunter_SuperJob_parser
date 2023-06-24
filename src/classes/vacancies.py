c0b3c1e8813e7676e2c3a99fc64ae12735298ed4
ilya <mrlivedance@vk.com>
2023-06-24 17:48:40 +0300

    Добавлен список all_list_vacancies в который при инициализации попадает словари с вакансиями

--- src/classes/vacancies.py
+++ src/classes/vacancies.py
@@ -1,14 +1,16 @@
 class Vacancies:
     """Класс вакансий"""
     all_instance_vacancies = []
-    __slots__ = ['format_dict_vacancy', '__name', '__url', '__salary', '__description']
+    all_list_vacancies = []
+    __slots__ = ['__format_dict_vacancy', '__name', '__url', '__salary', '__description']
 
-    def __init__(self, format_dict_vacancy: dict) -> None:
-        self.format_dict_vacancy = format_dict_vacancy
-        self.__name = self.format_dict_vacancy['name']
-        self.__url = self.format_dict_vacancy['url']
-        self.__salary = self.format_dict_vacancy['salary']
-        self.__description = self.format_dict_vacancy['description']
+    def __init__(self, format_dict_vacancy: dict = None) -> None:
+        self.__name = format_dict_vacancy['name']
+        self.__url = format_dict_vacancy['url']
+        self.__salary = format_dict_vacancy['salary']
+        self.__description = format_dict_vacancy['description']
+        self.all_instance_vacancies.append(self)
+        self.all_list_vacancies.append(format_dict_vacancy)
 
     def __str__(self) -> str:
         return f"Название: {self.__name}\n" \
@@ -16,9 +18,9 @@ class Vacancies:
                f"Краткое описание:\n" \
                f"{self.__description}...\n" \
                f"!Для полного отображения вакансии перейдите по ссылке! -> {self.__url}\n" \
-               f"{'_'* 150}"
+               f"{'_' * 150}"
 
     @staticmethod
-    def salary_comparison(vacancies_file: list) -> list:
+    def salary_comparison(vacancies_file: dict) -> list:
         sort_vacancies = sorted(vacancies_file, key=lambda x: x['salary'], reverse=True)
         return sort_vacancies
