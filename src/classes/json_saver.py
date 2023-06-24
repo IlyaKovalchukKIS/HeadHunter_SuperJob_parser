668b6586fa3ff1849d5b4f0594d6ba42ba29e283
ilya <mrlivedance@vk.com>
2023-06-24 12:39:04 +0300

    Добавлен метод загрузки данных из файла с вакансиями load_vacancies_json, изменены методы на @classmethod

--- src/classes/json_saver.py
+++ src/classes/json_saver.py
@@ -6,22 +6,26 @@ from abstract_classes import Saver
 
 class JsonSaver(Saver):
     """Класс для подключения по API к SuperJob"""
-    list_vacancies = []
     dir_path = os.path.abspath('vacancies_file/vacancies.json')
 
-    def __init__(self, vacancy: dict):
-        self.list_vacancies.append(vacancy)
+    @classmethod
+    def load_vacancies_json(cls) -> dict:
+        """Метод для загрузки вакансий из файла JSON"""
+        with open(cls.dir_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
 
-    def to_json(self) -> None:
+    @classmethod
+    def to_json(cls, all_vacancies: list) -> None:
         """Метод для сохранения вакансий в файл"""
-        with open(self.dir_path, 'w', encoding='UTF-8') as f:
-            json.dump(self.list_vacancies, f, indent=4, ensure_ascii=False)
+        with open(cls.dir_path, 'w', encoding='UTF-8') as f:
+            json.dump(all_vacancies, f, indent=4, ensure_ascii=False)
 
-    def del_vacancies(self, index: int) -> None:
+    @classmethod
+    def del_vacancies(cls, index: int) -> None:
         """Метод для удаления вакансий из файла"""
-        with open(self.dir_path, 'r') as f:
+        with open(cls.dir_path, 'r') as f:
             vacancies = json.load(f)
         del vacancies[int(index) - 1]
 
-        with open(self.dir_path, 'w') as f:
+        with open(cls.dir_path, 'w') as f:
             json.dump(vacancies, f, indent=4, ensure_ascii=False)
